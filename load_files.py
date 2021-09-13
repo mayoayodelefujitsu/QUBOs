08a9d7702c01006a24be650ecc78d0a793120d7b
mayoayodele <mayoayodele@gmail.com>
2021-09-13 14:54:11 +0100

    first commit

--- load_files.py
+++ load_files.py
@@ -1,11 +1,16 @@
 import numpy as np
 
-path = 'TSP//bays29.npz'
+path = 'TSP//gr17.npz'
 
 data = np.load(path)
-cost_function = data['objective']
-constraint_function = data['constraint']
-print(cost_function)
-print(constraint_function)
+cost_function_qubo_matrix = data['cost_function_qubo']
+cost_function_constant= data['cost_function_constant']
+constraint_function_qubo_matrix = data['constraint_function_qubo']
+constraint_function_constant= data['constraint_function_constant']
+
+print('constant term and QUBO matrix representing the cost (unconstrained objective) function' )
+print(cost_function_constant, cost_function_qubo_matrix)
+print('constant term and QUBO matrix representing the constraint function' )
+print(constraint_function_constant, constraint_function_qubo_matrix)
 
 
